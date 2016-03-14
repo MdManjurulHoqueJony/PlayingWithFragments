b677dd1ada204dbca422d58f87a27d79de091b64
MdManjurulHoqueJony <kucse03jony@gmail.com>
2016-03-14 23:43:58 +0600

    Initial commit

---
+++ app/src/main/java/com/wordpress/jonyonandroidcraftsmanship/playingwithfragments/Test2Fragment.java
@@ -0,0 +1,23 @@
+package com.wordpress.jonyonandroidcraftsmanship.playingwithfragments;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Test2Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        textView.setTextColor(Color.CYAN);
+        return textView;
+    }
+
+}
---
+++ app/src/main/java/com/wordpress/jonyonandroidcraftsmanship/playingwithfragments/Test3Fragment.java
@@ -0,0 +1,19 @@
+package com.wordpress.jonyonandroidcraftsmanship.playingwithfragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Test3Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_test3, container, false);
+    }
+
+}
